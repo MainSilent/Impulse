9e9e6e298af1623ae8fe54f842d56c35d20a8e30
MainSilent <70273681+MainSilent@users.noreply.github.com>
2022-01-22 15:43:29 +0330

    get challenge label

--- solver.py
+++ solver.py
@@ -1,10 +1,17 @@
 import os
+import re
 from yolov5 import predict
+from confusables import normalize
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 weights_path = os.path.join(os.path.dirname(__file__), "yolov5/yolov5x6.pt")
+
+labels = [
+    'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant'
+]
 
 class Solver():
     timeout = 10
@@ -31,6 +38,21 @@ class Solver():
             EC.presence_of_element_located((By.CSS_SELECTOR, '#checkbox'))
         ).click()
         self.driver.switch_to.default_content()
+
+        # Get challenge label
+        iframe = WebDriverWait(self.driver, self.timeout).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe[src *= "hcaptcha-challenge.html"]'))
+        )
+        self.driver.switch_to.frame(iframe)
+        raw_label = WebDriverWait(self.driver, self.timeout).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.prompt-text'))
+        ).text
+
+        raw_label = normalize(raw_label, prioritize_alpha=True)[0].lower()
+
+        for label in labels:
+            if label in raw_label:
+                print(label)
 
     def recaptcha(self):
         ...
