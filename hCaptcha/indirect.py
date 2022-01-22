b21e300de70e6f14574cee4a75bc82dc5c1ce0ef
MainSilent <70273681+MainSilent@users.noreply.github.com>
2022-01-22 08:58:36 +0330

    add logging to indirect captcha

--- hCaptcha/indirect.py
+++ hCaptcha/indirect.py
@@ -2,8 +2,13 @@ import os
 import time
 import json
 import gzip
+import logging
 from seleniumwire.webdriver import ChromeOptions
 from seleniumwire.undetected_chromedriver import Chrome
+
+logging.getLogger("seleniumwire").setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 key = ""
 host = ""
@@ -32,7 +37,7 @@ def response_interceptor(request, response):
 		driver.close()
 
 def new(_host, sitekey, chromedriver_path="/bin/chromedriver", headless=True):
-	global driver
+	global driver, host
 	host = _host
 
 	if not os.path.exists(chromedriver_path):
@@ -43,7 +48,7 @@ def new(_host, sitekey, chromedriver_path="/bin/chromedriver", headless=True):
 	driver = Chrome(executable_path=chromedriver_path, options=options)
 	driver.request_interceptor = request_interceptor
 	driver.response_interceptor = response_interceptor
-	print("Loading page...")
+	log.info("Loading page...")
 
 	page_content = f'''
 		<script src="https://hcaptcha.com/1/api.js" async defer></script>
@@ -51,18 +56,18 @@ def new(_host, sitekey, chromedriver_path="/bin/chromedriver", headless=True):
 	'''
 	driver.get(f"data:text/html;charset=utf-8,{page_content}")
 
-	print("Waiting for checkbox...")
+	log.info("Waiting for checkbox...")
 	while True:
 		try:
 			driver.switch_to.frame(0)
 			driver.find_element_by_id("checkbox").click()
-			print("Checkbox clicked")
+			log.info("Checkbox clicked")
 			break
 		except Exception as e:
 			driver.switch_to.default_content()
 			time.sleep(0.2)
 
-	print("Waiting for key...")
+	log.info("Waiting for key...")
 	while True:
 		if key != "":
 			return key
